df425d5f5994c4ca084fa71c96b442a1b0029eaf
tolujacob05 <tolujacob05@gmail.com>
2022-12-01 17:10:31 +0000

    complete

--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -27,28 +27,6 @@ const Main = () => {
                     </Routes>
                 </div>
 
-                <div className={styles.store1}>
-                <div className="retailer">
-                    <img src={pic6} alt="stores" />
-                    <div className="text">
-                        <h1> Location: Ajah, Lagos <br/> Name: Nao Supermarket <br /> About: We sell all sort of provisions, any kind of perfumes and deodorants and varieties of kitchen utensils </h1>
-                    </div>
-                </div>
-                <div >
-                    <img src={pic6} alt="stores" />
-                    <div className="text">
-                        <h1> Location: Lekki Phase 1, Lagos <br /> Name: Tech Heads <br /> About: We sell every kind of gadjets you can think of, game console included.</h1>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.house}>
-                <img src={pic6} alt="stores" />
-                <div></div>
-                <div className="tail">
-                    <img src={pic6} alt="stores" />
-                </div>
-            </div>
-
             </div>
         </HashRouter>
     );
